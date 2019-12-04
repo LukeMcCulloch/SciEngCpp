be4cfcdf6b68f6527b6613474d01519169a9c683
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-03 23:02:40 -0700

    playing with includes

--- include/point.h
+++ include/point.h
@@ -17,22 +17,3 @@ private:
    Number the_x;                // x-coordinate
    Number the_y;                // y-coordinate
 };
-
-Point::Point() {
-}
-
-Point::Point(Number x, Number y) {
-   the_x = x;
-   the_y = y;
-}
-
-Number Point::x() {return the_x;}
-Number Point::y() {return the_y;}
-
-Number Point::distance(Point point) {
-   Number x_diff = the_x - point.the_x;
-   Number y_diff = the_y - point.the_y;
-   return sqrt(x_diff * x_diff + y_diff * y_diff);
-}
-
-//Number Point::distance(Line line) {return line.distance(*this);}
