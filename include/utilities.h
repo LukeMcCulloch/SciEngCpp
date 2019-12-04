fd3d0f1050533a306bb077f4897a05d294927ff1
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-03 23:58:03 -0700

    basics of point and line class done

--- include/utilities.h
+++ include/utilities.h
@@ -1,4 +1,5 @@
 
 #include <cmath>
+#include <float.h>
 
 typedef float Number;
--- src/line.cpp
+++ src/line.cpp
@@ -19,3 +19,21 @@ Number Line::distance(Point point) {
    // returns the distance from point to the line
    return abs(a * point.x() +b * point.y() + c)/sqrt(a*a + b*b);
 }
+
+Number Line::parallelism_tolerance = .01745; // 1 deg in radians
+
+Point Line::intersect(Line line) {
+   // Intersect with line.
+   // if angle between the two is < tol, then point at infinity
+   // here we check the square of the sum of the angle
+   // for tol <<, sin theta = theta
+   Number det = a * line.b - line.a * b;
+   Number sinsq = (det * det) / ((a*a + b*b) * (line.a*line.a +line.b*line.b) ) ;
+   if (sinsq < parallelism_tolerance * parallelism_tolerance) {
+      return Point(FLT_MAX, FLT_MAX);   // point at infinity (FLT_MAX from float.h)
+   }
+   else{
+      return Point( (b*line.c - line.b*c)/det ,
+                     (c*line.a - line.c*a) / det);
+   }
+}
