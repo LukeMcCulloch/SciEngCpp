107dd674495972a04691de391e59e8a9ab2d2298
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-03 22:31:27 -0700

    line class, point class, and a new typedef Number in utilities

---
+++ src/line.cpp
@@ -0,0 +1,20 @@
+#include "utilities.h"
+
+class Point; //fwd declaration
+
+class Line {
+
+public:
+   Line(Point p1, Point p2);                 // Line through two points
+   Line(Point p, Number xDir, Number yDir);  // Line through p with tangent <xDir, yDir>
+   Point intersect(Line line);               // Intersection with a line
+   Number distance(Point point);             // Distance to a point
+
+   static Number parallelism_tolerance;      // Smallest nonparallel angle (radians)
+
+private:
+   //ax + by + c = 0
+   Number a;
+   Number b;
+   Number c;
+};
