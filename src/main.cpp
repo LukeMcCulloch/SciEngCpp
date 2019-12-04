3265dfb277787aafe3b76ba3a44cf519c5dd401a
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-03 20:47:08 -0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <cassert>
+#include <iostream>
+#include <limits>
+
+#include <cmath>
+
+//#include "xxx.hpp"
+
+
+
+using namespace std;
+using std::cout;
+using std::endl;
+
+
+
+
+
+//int etarraybasic (SolverManager& SM){
+int main (){
+
+    cout << "hello world!" << endl;
+    
+    return 0;
+
+}
