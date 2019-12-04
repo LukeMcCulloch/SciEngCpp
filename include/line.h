30964a2d5ccc2632419c6ac67e43a14d841470da
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-03 23:03:25 -0700

    playing with includes

--- include/line.h
+++ include/line.h
@@ -19,22 +19,3 @@ private:
    Number b;
    Number c;
 };
-
-Line::Line(Point p1, Point p2) {
-   //construct a line through points p1 and p2
-   if (p1.x() == p2.x()) {   // the line is vertical
-      a = 1;
-      b = 0;
-      c = -p1.x();
-   }
-   else {                    // nonvertical line
-      a = p2.y() - p1.y();
-      b = p2.x() - p1.x();
-      c = p1.y() * p2.x() - p2.y() * p1.x();
-   }
-}
-
-Number Line::distance(Point point) {
-   // returns the distance from point to the line
-   return abs(a * point.x() +b * point.y() + c)/sqrt(a*a + b*b);
-}
