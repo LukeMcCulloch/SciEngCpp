f3685b86b505d9655269b8e5763a50c5286a019f
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-08 19:52:21 -0700

    B&N mesh lib soluion 2

---
+++ src/mesh.cpp
@@ -0,0 +1,14 @@
+#include <cassert>
+#include <iostream>
+#include <limits>
+#include <cmath>
+
+
+#include "utilities.h"
+#include "point.h"
+#include "line.h"
+#include "SimpleArray.h"
+#include "SimpleArray.cpp" 
+
+typedef Point Node;        //Node class that behaves like a point
+
