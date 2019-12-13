c76a0f5440d06764b24ad000fd0be3e6490e5ed0
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-12 23:21:07 -0700

    mesh class

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -15,8 +15,16 @@
 
 #include "mesh.hpp"
 
+typedef Point Node; 
+
 class Mesh {
 public:
    bool checkElementAngles(Number angle_threshold) const;
    Number maxAngle(const Element& e) const;
+   int numNodes() const;
+   int numElements() const;
+   friend std::istream& operator>>(std::istream&, Mesh&);
+private:
+   SimpleArray<Node>    node_table;
+   SimpleArray<Element> elemetn_table;
 };
